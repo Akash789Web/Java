e642615baa132f90c730e26e0f04a22ad511450f
Akash789Web <wilsonakash9@gmail.com>
2026-02-07 12:13:21 +0530

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,16 @@
+public class SimpleSum {
+    public static void main(String[] args) {
+        // 1. Declare variables
+        int number1 = 10;
+        int number2 = 20;
+
+        // 2. Perform the math
+        int sum = number1 + number2;
+
+        // 3. Print the result
+        System.out.println("The sum is: " + sum);
+    }
+}
+
+
+
